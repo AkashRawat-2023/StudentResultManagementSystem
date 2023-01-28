405316f4e72718f90854dbf23a4e249ed824f225
aki11 <aki11@LAPTOP-DFHG4H1I>
2023-01-28 17:15:26 +0530

    Student Result Management System

---
+++ src/student/result/management_system/StudentResultManagement_System.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package student.result.management_system;
+
+/**
+ *
+ * @author aki11
+ */
+public class StudentResultManagement_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
